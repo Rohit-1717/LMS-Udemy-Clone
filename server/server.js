d0792d1b15f50aa12d8cf7a533587cd6a3187593
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2024-10-20 00:33:05 +0530

    MondoDB Setup Completed

--- server/server.js
+++ server/server.js
@@ -1,9 +1,40 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const mongoose = require("mongoose");
 
-app.use(cors());
+const PORT = process.env.PORT || 5000;
+const MongoDB_URI = process.env.MONGODB_URI;
+
+cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
+
+app.use(express.json());
+
+// Database connection
+mongoose
+  .connect(MongoDB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 //! last endpoint  || 24:17 / 12:18:42 ||
